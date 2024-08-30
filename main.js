fc4f6f41cdefc35264e67ead94ac560efcaeebf6
shaizan1213 <shaizan.alobha@gmail.com>
2024-08-30 18:37:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+const swiper = new Swiper('.hero-section .swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 1000,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Swiper(".blog-section .mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 3000,
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
+
